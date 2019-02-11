5c88a34da56b9642d129516d3589005a20f2f219
Ben Corman <benjamincorman@gmail.com>
2019-02-10 16:52:28 -0800

    8 tutorial questions added

--- scripts/repl.js
+++ scripts/repl.js
@@ -5,6 +5,71 @@ const resultsNode = document.getElementById('results');
 const selectorDisplay = document.getElementById('selector-display');
 const copyButton = document.getElementById('copy-button');
 const promptButton = document.getElementById('prompt-button');
+const promptText = document.getElementById('prompt');
+
+let currentPrompt = Number(localStorage.getItem('currentPrompt'));
+
+const prompts = [
+  {
+    text: `Trigger if the fruit variable has the value 'banana'.`,
+    code: `var fruit = 'banana';`,
+  },
+  {
+    text: `Trigger if the code is printing a string instead of an identifier, 
+      for example: print('i') instead of print(i).`,
+    code: `print(identifier);\nprint('string')`,
+  },
+  {
+    text: `Trigger if the code has an if statement inside another if statement.`,
+    code: `if (x > 5) {\n  if (x === y) {\n    print(x);\n  }\n}`,
+  },
+  {
+    text: `Trigger if the code has an if statement checking that x and 3 are equal (allow commutation).`,
+    code: `if (x === 3) {\n  foo();\n}\n\nif (3 === x) {\n  bar();\n}`,
+  },
+  {
+    text: `Trigger if the num variable is a string instead of a number. Strings will always use single quotes.`,
+    code: `var num = '10';`,
+  },
+  {
+    text: `Trigger if the array contains only 'food', 'equipment', and 'clothing'.`,
+    code: `var goodBackpack = ['food', 'equipment', 'clothing'];
+var badBackpack = ['food', 'coal', 'cats', 'equipment', 'clothing'];
+var worseBackpack = ['garbage'];`,
+  },
+  {
+    text: `The user is expected to change the setup parameters of this classic for loop. Write set of queries and combine them into a 'resetNeeded' trigger that fires any other starter code has been changed.`,
+    code: `import { array } from 'otherCode';\n
+if (array.length) {
+  for (let i = 0; i < array.length; i = i + 1) {
+    console.log(array[i]);
+  }
+}`,
+  },
+  {
+    text: `Write a set of triggers to validate that this puzzle is complete`,
+    code: `// Starter Code
+for (var element of ['snap', 'crackle', 'pop']) {\n}\n
+// Solution Code
+for (var element of ['snap', 'crackle', 'pop']) {
+  if (element === 'pop') {
+    print('Get poppin');
+  }
+}`,
+  },
+];
+
+const cyclePrompt = (e) => {
+  e.preventDefault();
+  currentPrompt === prompts.length - 1 ? currentPrompt = 0 : currentPrompt++;
+  localStorage.setItem('currentPrompt', currentPrompt);
+  updatePrompt();
+};
+
+const updatePrompt = () => {
+  codeConsole.innerHTML = prompts[currentPrompt].code;
+  promptText.innerHTML = `<h3 class="prompt-text">${currentPrompt + 1} of ${prompts.length} ${prompts[currentPrompt].text}</h3>`;
+};
 
 const copyConfirmation = () => {
   const copyMessage = document.getElementById('copy-message');
@@ -29,11 +94,11 @@ const update = () => {
   let ast;
 
   try {
-    ast = esprima.parse(codeConsole.value, { sourceType: 'module' });
+    ast = esprima.parse(codeConsole.value, {sourceType: 'module'});
   } catch (e) {
     isSourceValid = false;
   }
-  const selector = selectorNode.value.replace(/\n/g, ''); // remove line breaks from query string
+  const selector = selectorNode.value.replace(/\n/g, '');
   outputConsole.innerHTML = '';
 
   let start; let end; let selectorAst; let matches; let matchesOutput;
@@ -45,7 +110,7 @@ const update = () => {
   }
 
   try {
-    selectorAst = esquery.parse(selector, { sourceType: 'module' });
+    selectorAst = esquery.parse(selector, {sourceType: 'module'});
   } catch (e) {
     isSelectorValid = false;
   }
@@ -68,7 +133,9 @@ const update = () => {
   const duration = Math.round((end - start) * (10 ** 2)) / (10 ** 2);
   const invalidSource = `<span id='numMatches'></span>Invalid Source Code`;
   const invalidSelector = `<span id='numMatches'></span>Invalid Selector`;
-  const resultsMessage = `<span id='numMatches'>${numMatches}</span> node${numMatches === 1 ? '' : 's'} found in ${duration} ms`;
+  const resultsMessage =
+    `<span id='numMatches'>${numMatches}</span> 
+    node${numMatches === 1 ? '' : 's'} found in ${duration} ms`;
 
   if (isSourceValid && isSelectorValid) {
     resultsNode.innerHTML = resultsMessage;
@@ -99,11 +166,19 @@ const update = () => {
   outputConsole.innerHTML = matchesOutput;
 };
 
-update();
-
 codeConsole.addEventListener('change', update);
 codeConsole.addEventListener('keyup', update);
 selectorNode.addEventListener('change', update);
 selectorNode.addEventListener('keyup', update);
 copyButton.addEventListener('mousedown', copyQuery);
-promptButton.addEventListener('click', showPrompt);
+promptButton.addEventListener('mousedown', cyclePrompt);
+promptButton.addEventListener('mousedown', update);
+
+if (!currentPrompt) {
+  localStorage.setItem('currentPrompt', 0);
+  currentPrompt = 0;
+} else {
+  updatePrompt();
+};
+
+update();
